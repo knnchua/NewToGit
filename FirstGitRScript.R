1efdbb3e223ea6ece70dbb402e8fdc75fa72586a
Kenn Chua <chuax025@umn.edu>
2018-11-02 01:03:35 -0500

    First commit with notes on R-Git

---
+++ FirstGitRScript.R
@@ -0,0 +1,11 @@
+
+
+#A. Setting up Git
+##1. Go to Github page; create account; create new repository
+##2. Go to Applications > Utilities > Terminal
+##3. Set directory using: cd ~/Google\ Drive/Git
+##4. Type -ls- to show files in directory
+##5. Type -git clone <link to github repository> 
+
+
+#B. Connectin RStudio with Git
