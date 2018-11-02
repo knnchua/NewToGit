396c24263f8579027d3484e99d6d427295fd4773
Kenn Chua <chuax025@umn.edu>
2018-11-02 01:08:39 -0500

    Second commit. With more instructions.

--- FirstGitRScript.R
+++ FirstGitRScript.R
@@ -1,6 +1,6 @@
 
 
-#A. Setting up Git
+#A. Setting up Git [No need to do 2, 3, 4, 5 for R]
 ##1. Go to Github page; create account; create new repository
 ##2. Go to Applications > Utilities > Terminal
 ##3. Set directory using: cd ~/Google\ Drive/Git
@@ -8,4 +8,13 @@
 ##5. Type -git clone <link to github repository> 
 
 
-#B. Connectin RStudio with Git
+#B. Connecting RStudio with Git
+##1. File > New Project > Version Control > Git > Paste link of repository
+##2. In Git tab on upper right window, check Paths then click commit.
+##3. Clicking commit uploads the paths to the repository. 
+##4. On the new window, check out files.
+##5. Click push to push the changes. 
+
+x <-1 
+y <-7
+x+y
